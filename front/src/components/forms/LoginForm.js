46b427f0f8823fb884349960932f18f6e2f648e8
Basia <bachna22@gmail.com>
2020-04-03 18:54:00 +0200

    Created login form component

---
+++ front/src/components/forms/LoginForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { TextInput, Button, Icon } from "react-materialize";
+import "./form.css";
+
+export default class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.saveUsername = this.saveUsername.bind(this);
+    this.updateState = this.updateState.bind(this);
+
+    this.state = {
+      username: ""
+    };
+  }
+
+  updateState(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  saveUsername() {
+    localStorage.setItem("username", this.state.username);
+  }
+
+  render() {
+    return (
+      <div className="center form-box login border">
+        <TextInput onChange={this.updateState} label="Username" />
+        <div className="center">
+          <Button
+            node="button"
+            type="submit"
+            waves="light"
+            onClick={this.saveUsername}
+          >
+            Play
+            <Icon right>send</Icon>
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
