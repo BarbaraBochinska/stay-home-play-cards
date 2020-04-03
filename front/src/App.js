b141cb4be69f9957a43245ff19b3e5e09743ba4a
Basia <bachna22@gmail.com>
2020-04-03 18:53:15 +0200

    Update

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import LoginForm from "./components/forms/LoginForm";
 
 export default class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <LoginForm></LoginForm>
+      </div>
+    );
   }
 }
