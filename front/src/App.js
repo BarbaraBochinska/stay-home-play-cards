f0d9957c86b1b6e340daf5f4f619f548260ebd67
Basia <bachna22@gmail.com>
2020-04-11 13:50:27 +0200

    Created routes, added link for play btn

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import LoginForm from "./components/forms/LoginForm";
+import Nav from "./components/navbar/Nav";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <LoginForm></LoginForm>
-      </div>
+      <Router>
+        <Route exact path="/" component={LoginForm} />
+        <Route path="/table" component={Nav} />
+      </Router>
     );
   }
 }
